1891b91ba57a83f236a1932e1518e0a12909ab55
Augustin-Zidek <augustin@zidek.eu>
2022-07-03 19:52:04 +0100

    Small usability improvements:
    * Show the match number when the user runs
    
    * Show all warnings in red colour
    
    * Better input handling and command history via arrows
    
    * Better error messages for bad player names input
    
    * Small documentation updates

--- Matboj.py
+++ Matboj.py
@@ -2,6 +2,7 @@
 import json
 import os
 import re
+import readline  # For a nice UNIX-like command line within the program.
 import sys
 
 
@@ -49,6 +50,10 @@ def print_list(lst, cols=3):
     print(' │ '.join(elements))
 
 
+def print_error(text):
+  print(f'\033[91m{text}\033[0m')
+
+
 def new_ranks(winner, loser):
   new_winner_rank = winner.rank + 100 - (winner.rank - loser.rank) / 3
   new_loser_rank = loser.rank - 100 - (loser.rank - winner.rank) / 3
@@ -64,16 +69,16 @@ class Matboj(object):
 
   def save_game_state(self):
     dirname = os.path.dirname(__file__)
-    with open(os.path.join(dirname, 'Game_state.json'), 'w') as output:
+    with open(os.path.join(dirname, 'game_state.json'), 'w') as output:
       game_state = {
           'people': [p.to_dict() for p in
                      sorted(self._people, key=lambda e: -e.rank)],
           'match_list': self._match_list}
-      output.write(json.dumps(game_state, indent='  '))
+      output.write(json.dumps(game_state, indent=2))
 
   def load_game_state(self):
     dirname = os.path.dirname(__file__)
-    with open(os.path.join(dirname, 'Game_state.json'), 'r') as game_state:
+    with open(os.path.join(dirname, 'game_state.json'), 'r') as game_state:
       game_state = json.loads(game_state.read())
       self._people = [Person.from_dict(p) for p in game_state['people']]
       self._names_to_people = {p.name: p for p in self._people}
@@ -92,17 +97,16 @@ class Matboj(object):
 
   def undo(self, position):
     if not self._match_list:
-      print('Nothing to undo.')
+      print_error('Nothing to undo.')
       return
 
     if position > len(self._match_list):
-      print('Not enough matches (%d) to undo position match at position -%d'
-            % (len(self._match_list), position))
+      print_error(f'Not enough matches ({len(self._match_list)}) to undo '
+                  f'position match at position -{position}')
       return
 
     undo_winner_name, undo_loser_name = self._match_list[-position]
-    print('Undo %s:%s? [y/n] ' % (undo_winner_name, undo_loser_name), end='')
-    answer = input().strip()
+    answer = input(f'Undo {undo_winner_name}:{undo_loser_name}? [y/n] ').strip()
     if answer == 'y':
       # Undo all the matches up to the bad one, then redo them since somebody
       # has maybe already played with the players whose match we are undoing.
@@ -117,43 +121,45 @@ class Matboj(object):
       # Redo the matches.
       for redo_match in redo_buffer:
         self.rerank(*redo_match)
-      print('Undo done for %s:%s' % (undo_winner_name, undo_loser_name))
+      print(f'Undo done for {undo_winner_name}:{undo_loser_name}')
       self.print_ranking()
 
 
-  def rerank(self, name1, name2):
-    if name1 in self._names_to_people and name2 in self._names_to_people:
-      winner = self._names_to_people[name1.lower()]
-      loser = self._names_to_people[name2.lower()]
-      new_winner_rank, new_loser_rank = new_ranks(
-          winner=winner, loser=loser)
+  def rerank(self, winner_name, loser_name):
+    if winner_name not in self._names_to_people:
+      print_error(f'Unknown winner name: {winner_name.title()}.')
+    elif loser_name not in self._names_to_people:
+      print_error(f'Unknown loser name: {loser_name.title()}.')
+    else:
+      winner = self._names_to_people[winner_name.lower()]
+      loser = self._names_to_people[loser_name.lower()]
+      new_winner_rank, new_loser_rank = new_ranks(winner=winner, loser=loser)
       winner.update_rank(new_winner_rank)
       loser.update_rank(new_loser_rank)
       self._match_list.append((winner.name, loser.name))
       self.save_game_state()
-    else:
-      print('Unknown winner or loser name.')
+
 
   def run(self):
     self.print_ranking()
 
     print('Welcome to Matboj! Type "help" to show available commands.')
     while True:
-      print('\033[94m$\033[0m\033[92m ', end='')
-      command = input().strip()
+      command = input('\033[94m$\033[0m\033[92m ').strip()
       print('\033[0m', end='')
 
       if command == '':
         continue
       if command == 'about':
         print('Written by Augustin Zidek in 2020.')
-      elif command in ['quit', 'exit']:
+      elif command in ('quit', 'exit', ':q!'):
         self.save_game_state()
         exit()
-      elif command in ['status', 'print']:
+      elif command in ('status', 'print'):
         self.print_ranking()
       elif command == 'matches':
-        print(', '.join(['%s:%s' % (w, l) for w, l in self._match_list]))
+        print(', '.join([f'{w}:{l} ({len(self._match_list) - n})'
+                         for n, (w, l) in enumerate(self._match_list)]))
       elif command == 'save':
         self.save_game_state()
         print('Game state saved.')
@@ -170,16 +176,19 @@ class Matboj(object):
         undo_position = int(re.match('undo ([0-9]+)', command).group(1))
         self.undo(position=undo_position)
       elif re.match(r'\w+:\w+', command):
-        name1, name2 = command.split(':')
-        self.rerank(name1, name2)
+        winner_name, loser_name = command.split(':')
+        if winner_name == loser_name:
+          print_error('The two player names must be distinct.')
+          continue
+        self.rerank(winner_name=winner_name, loser_name=loser_name)
         self.print_ranking()
       else:
-        print('Unknown command.')
+        print_error('Unknown command.')
 
 
 def main(unused_args):
   dirname = os.path.dirname(__file__)
-  people = load_people(os.path.join(dirname, 'People.txt'))
+  people = load_people(os.path.join(dirname, 'people.txt'))
 
   matboj = Matboj(people)
   matboj.run()
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Matboj evaluator
 
-Used at math camps like Pikomat or KMS for keeping score when all participants
-in the camp play a board game and need to compute individual ranks periodically.
+Used at math camps like [Pikomat](https://www.pikomat.sk/) or
+[KMS](https://kms.sk/) for keeping score when all participants in the camp play
+a board game and need to compute individual ranks periodically.
 
 ## See it in action
 
@@ -9,28 +10,26 @@ in the camp play a board game and need to compute individual ranks periodically.
 
 ## Features
 
-* Print the current score or matches that have been played so far.
+* Print the current scores or matches that have been played so far.
 * Undo any match that has been played and recorded incorrectly.
 * Save the game state in a JSON file and restore from it at any other time.
-* Written as a command-line app which makes it extremely ergonomic to use and
-  the user can handle 40+ kids playing at the same time.
+* Written as a command-line app which makes it extremely ergonomic to use. The
+  user can handle 40+ people playing matches at the same time.
 * No setup and dependencies needed, only a Python 3 installation.
-* Short (< 300 lines of code), so easy to hack and customize.
+* Short (~200 lines of code), so easy to hack and customize.
+* Battle tested at a few events.
 * Permissive MIT license.
 
 ## Using the evaluator
 
-Put names of all the contestants in the `People.txt` file, one name per line.
+Put names of all the contestants in the `people.txt` file, one name per line.
 All the names have to be unique and they are case-sensitive.
 
 Then launch the application from the terminal:
 
 ```bash
-python Matboj.py
+python3 matboj.py
 ```
-
-Note that the app is Python 3+, so you might have to use the `python3` binary if
-the `python` binary still points to to Python 2 on your system.
 
 If you are using the app interactively, increase the text size in your terminal
 and use an overhead projector to show the players what their scores are.
